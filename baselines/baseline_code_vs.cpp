7dee83bad8aebbc1220247296c21adaff50b5507
Nick Schoeb <nickschoeb@users.noreply.github.com>
2024-06-03 21:36:46 -0400

    Create baseline_code_vs.cpp

---
+++ baselines/baseline_code_vs.cpp
@@ -0,0 +1,25 @@
+/*
+Author:			Your Name
+Description:	Your Description
+*/
+
+// Allowed Warnings
+#pragma warning (disable:6031) // getch
+#pragma warning (disable:4996) // scanf
+
+// C
+#include <stdio.h>
+#include <conio.h>
+
+// C++
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+	// add your code here
+
+	// Exit to OS
+	_getch();
+	return 0;
+}
