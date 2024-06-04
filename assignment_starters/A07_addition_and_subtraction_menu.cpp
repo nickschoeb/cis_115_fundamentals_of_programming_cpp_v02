ee9ae63a8f2353e12fe5bf489131cfffb22e084e
Nick Schoeb <nickschoeb@users.noreply.github.com>
2024-06-03 22:00:16 -0400

    Create A07_addition_and_subtraction_menu.cpp

---
+++ assignment_starters/A07_addition_and_subtraction_menu.cpp
@@ -0,0 +1,29 @@
+// PREPROCESSOR DIRECTIVE AND SETUP
+#pragma warning(disable:6031)
+#include <iostream>
+#include <iomanip>
+#include <conio.h>
+using namespace std;
+
+// CONSTANTS
+
+// PROTOTYPES
+
+// DRIVER FUNCTION DEFINITION
+int main() {
+	// Declare local variables.
+
+	// Prompt for 'firstOperand'.
+
+	// Prompt for 'secondOperand'.
+
+	// Prompt for an operation (addition or subtraction).
+
+	// Display the proper equation by calling a function.
+
+	// Indicate normal termination to OS.
+	_getch();
+	return 0;
+}
+
+// FUNCTION DEFINITIONS
