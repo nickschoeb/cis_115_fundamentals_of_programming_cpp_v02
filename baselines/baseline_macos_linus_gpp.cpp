fc75240c684eeec0023f448514689586e6834631
Nick Schoeb <nickschoeb@users.noreply.github.com>
2024-06-03 21:39:50 -0400

    Create baseline_macos_linus_gpp.cpp

---
+++ baselines/baseline_macos_linus_gpp.cpp
@@ -0,0 +1,23 @@
+/*
+Author:         Your Name
+Description:    Baseline XCode/macOS. Linux, or g++
+*/
+
+// Allowed Warnings
+
+// C
+#include <stdio.h>
+
+// C++
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    // add your code here
+    
+    // Exit to OS
+    rewind(stdin);
+    getchar();
+    return 0;
+}
