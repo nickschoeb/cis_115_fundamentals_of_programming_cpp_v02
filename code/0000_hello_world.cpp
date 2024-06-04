b88836502ccbd6dac7dc7af9d87f22ebd019d8d6
Nick Schoeb <nickschoeb@users.noreply.github.com>
2024-06-03 21:40:56 -0400

    Create 0000_hello_world.cpp

---
+++ code/0000_hello_world.cpp
@@ -0,0 +1,13 @@
+/* 
+Author      : Mark Dencler
+Description : An introductory program that displays the message "Hello World"
+              on the screen.
+*/
+
+#include <stdio.h>
+
+int main() {
+	printf("Hello World.\n");
+
+	return 0;
+}
