d10786856c120201e7301018f1c6bda10b459964
Nick Schoeb <nickschoeb@users.noreply.github.com>
2024-06-03 22:05:46 -0400

    Create a08_basic_arrays.cpp

---
+++ assignment_starters/a08_basic_arrays.cpp
@@ -0,0 +1,29 @@
+// Including libraries and external dependencies
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+#define CAPACITY 300
+
+int main() {
+	// Declare a character array capable of holding 'CAPACITY' elements
+	//   and initialize it to a default message.
+	char theMessage[CAPACITY] = "This is the test message.  It has a couple sentences in it.";
+	int counter = 0;
+
+	// 1.) Display the contents of the array one character at a time using a loop.
+
+	// 2.) Use a loop to count the number of vowels in the sentence and display
+	//   the results on the screen.
+
+	// 3.) Write a loop that moves through the array and converts every lower-case letter
+	//   to it's upper-case equivalent.  You may not use a standard library function
+	//   to accomplish this task, just loops and assignment.  Display a confirmation
+	//   message on the screen once the conversion is complete.
+
+	// 4.) Display the contents of the array with a puts() call.
+	
+	// It's Over!
+	_getch();
+	return 0;
+}
